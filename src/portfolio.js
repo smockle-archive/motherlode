1ee990b050d9d0168bb8c45fe5e5557d106bd42c
Clay Miller <clay@smockle.com>
2015-10-18 15:54:49 -0700

    Rename Motherlode to Portfolio for clarity.

---
+++ src/portfolio.js
@@ -0,0 +1,92 @@
+import Asset from './asset';
+import Percent from './percent';
+import USD from './usd'; const $ = USD;
+
+// Symbols, current prices, current number of shares, ideal allocation in memory
+  // TODO: Look up current prices
+  // TODO: Ask for symbols, current number of shares, ideal allocation (or read from file)
+
+// Calculate current allocation
+
+// Calculate difference between current allocation and ideal allocation for each symbol
+
+function Portfolio(assets) {
+  let portfolio = Object.create(Portfolio.prototype);
+
+  if (!(assets instanceof Array))
+    throw new TypeError('Expected input to be an array.');
+  if (assets.length === 0)
+    throw new Error('Input array cannot be empty.');
+
+  // Calculate net and validate each element in input array.
+  portfolio.net = $(0);
+  assets.forEach((u) => {
+    if (!u.hasOwnProperty('asset'))
+      throw new TypeError('Input array contains an element without an asset key.');
+    if (!u.hasOwnProperty('quantity'))
+      throw new TypeError('Input array contains an element without a quantity key.');
+    if (!u.hasOwnProperty('ideal'))
+      throw new TypeError('Input array contains an element without an ideal key.');
+    if (!(u.asset instanceof Asset))
+      throw new TypeError('Input array contains an element with a bad asset value.');
+    if (typeof u.quantity !== 'number' && !(u.quantity instanceof Number))
+      throw new TypeError('Input array contains an element with a bad quantity value.');
+    if (!(u.ideal instanceof Percent))
+      throw new TypeError('Input array contains an element with a bad ideal value.');
+    if (u.quantity < 0)
+      throw new TypeError('Input array contains an element with a negative quantity.');
+    portfolio.net += $(u.asset.price * u.quantity);
+  });
+
+  // Set allocation and delta on each element in input array.
+  assets.forEach((u) => {
+    u.allocation = Percent.fromDecimal(u.asset.price * u.quantity / portfolio.net);
+    u.delta = Percent.fromDecimal(u.allocation - u.ideal);
+  });
+
+  portfolio.assets = Object.assign(assets);
+  return portfolio;
+};
+
+// Ask how many more dollars we want to allocate
+  // TODO: Support deallocating [i.e. withdrawing] dollars
+  // TODO: Support changing ideal allocation with existing number of dollars
+
+// While dollars we want to allocate is greater than dollars we’ve allocated
+  // Remove as many dollars we want to allocate as it takes to purchase one share of the symbol with the greatest [negative] difference
+
+// Display current number of shares, recommended purchases of shares, and what the number of shares after purchase would be
+  // TODO: Save symbols, [new] current number of shares, ideal allocation to file
+
+Portfolio.prototype.load = function(amount) {
+  if (!(amount instanceof $))
+    throw new TypeError('Load amount must be in USD.');
+
+  this.net = $(this.net + amount);
+
+  const unallocated = Object.assign(this.assets);
+  const minPrice = this.assets.reduce((min, a) => Math.min(min, a.asset.price), Infinity);
+  while(amount >= minPrice) {
+    // Move asset with largest [negative] delta to the top.
+    // If multiple assets have the same delta, sort by price.
+    this.assets.sort((a, b) => {
+      return a.delta - b.delta === 0 ?
+      a.asset.price - b.asset.price :
+      a.delta - b.delta;
+    });
+    // Break if underallocated asset price is greater than remaining load amount.
+    if (this.assets[0].asset.price > amount) break;
+    this.assets[0].quantity++;
+    amount -= this.assets[0].asset.price;
+  }
+
+  // Store uninvestable load amount as _CASH.
+  if (amount > 0)
+    this.assets.push({
+      asset: Asset('_CASH', $(1)),
+      quantity: amount.valueOf(),
+      ideal: Percent(0)
+    });
+};
+
+export default Portfolio;
---
+++ test/portfolio.spec.js
@@ -0,0 +1,154 @@
+import 'mocha';
+import {assert} from 'chai';
+import Asset from '../src/asset';
+import Percent from '../src/percent';
+import USD from '../src/usd'; const $ = USD;
+import Portfolio from '../src/portfolio';
+
+describe('Portfolio', () => {
+  describe('#constructor', () => {
+    it('throws if input is not an Array', () => {
+      assert.throws(Portfolio.bind(Portfolio, 'ZVZZT'), TypeError);
+    });
+    it('throws if input Array is empty', () => {
+      assert.throws(Portfolio.bind(Portfolio, []), Error);
+    });
+    it('throws if input Array contains an element without a required key', () => {
+      const assets = [{
+        asset: Asset('ZVZZT', $(200)),
+        quantity: 1,
+        ideal: Percent(100)
+      }];
+      const assetless = Object.assign(assets); delete assetless[0].asset;
+      const quantityless = Object.assign(assets); delete quantityless[0].quantity;
+      const idealless = Object.assign(assets); delete idealless[0].ideal;
+      assert.throws(Portfolio.bind(Portfolio, [assetless]), TypeError);
+      assert.throws(Portfolio.bind(Portfolio, [quantityless]), TypeError);
+      assert.throws(Portfolio.bind(Portfolio, [idealless]), TypeError);
+    });
+    it('throws if input Array contains an element with a value of the wrong type', () => {
+      const assets = [{
+        asset: Asset('ZVZZT', $(200)),
+        quantity: 1,
+        ideal: Percent(100)
+      }];
+      let notasset = Object.assign(assets); notasset[0].asset = 'ZVZZT';
+      let notquantity = Object.assign(assets); notquantity[0].quantity = '1';
+      let notideal = Object.assign(assets); notideal[0].ideal = '100%';
+      assert.throws(Portfolio.bind(Portfolio, [notasset]), TypeError);
+      assert.throws(Portfolio.bind(Portfolio, [notquantity]), TypeError);
+      assert.throws(Portfolio.bind(Portfolio, [notideal]), TypeError);
+    });
+    it('throws if input Array contains an element has a negative quantity', () => {
+      const negative = [{
+        asset: Asset('ZVZZT', $(200)),
+        quantity: -1,
+        ideal: Percent(100)
+      }];
+      assert.throws(Portfolio.bind(Portfolio, [negative]), TypeError);
+    });
+    it('creates a Portfolio when all inputs are valid', () => {
+      const assets = [{
+        asset: Asset('ZVZZT', $(200)),
+        quantity: 1,
+        ideal: Percent(100)
+      }];
+      const portfolio = Portfolio(assets);
+      assert.instanceOf(portfolio, Portfolio);
+      assert.deepEqual($(200), $(portfolio.net));
+      assert.deepEqual(assets[0].asset, portfolio.assets[0].asset);
+      assert.strictEqual(assets[0].quantity, portfolio.assets[0].quantity);
+      assert.deepEqual(assets[0].ideal, portfolio.assets[0].ideal);
+    });
+  });
+  describe('#load', () => {
+    it('throws if input is not in USD', () => {
+      const assets = [{
+        asset: Asset('ZVZZT', $(200)),
+        quantity: 1,
+        ideal: Percent(100)
+      }];
+      let portfolio = Portfolio(assets);
+      assert.throws(portfolio.load.bind(portfolio, 'ZVZZT'), TypeError);
+    });
+    it('loads additional funds without cash surplus', () => {
+      const assets = [{
+        asset: Asset('ZVZZT', $(200)),
+        quantity: 1,
+        ideal: Percent(100)
+      }];
+      let portfolio = Portfolio(assets);
+      portfolio.load($(200));
+      // Check portfolio
+      assert.deepEqual(portfolio.net, $(400));
+      assert.strictEqual(portfolio.assets.length, 1);
+      // Check SPY
+      const zvzzt = portfolio.assets.find((a) => a.asset.symbol === 'ZVZZT');
+      assert.strictEqual(zvzzt.quantity, 2);
+    });
+    it('loads additional funds with cash surplus', () => {
+      const assets = [{
+        asset: Asset('ZVZZT', $(200)),
+        quantity: 1,
+        ideal: Percent(100)
+      }];
+      let portfolio = Portfolio(assets);
+      portfolio.load($(250));
+      // Check portfolio
+      assert.deepEqual(portfolio.net, $(450));
+      assert.strictEqual(portfolio.assets.length, 2);
+      // Check SPY
+      const zvzzt = portfolio.assets.find((a) => a.asset.symbol === 'ZVZZT');
+      assert.strictEqual(zvzzt.quantity, 2);
+      // Check CASH
+      const cash = portfolio.assets.find((a) => a.asset.symbol === '_CASH');
+      assert.deepEqual(cash.asset.price, $(1));
+      assert.strictEqual(cash.quantity, 50);
+    });
+    it('loads cash surplus', () => {
+      const assets = [{
+        asset: Asset('ZVZZT', $(200)),
+        quantity: 1,
+        ideal: Percent(100)
+      }];
+      let portfolio = Portfolio(assets);
+      portfolio.load($(50));
+      // Check portfolio
+      assert.deepEqual(portfolio.net, $(250));
+      assert.strictEqual(portfolio.assets.length, 2);
+      // Check SPY
+      const zvzzt = portfolio.assets.find((a) => a.asset.symbol === 'ZVZZT');
+      assert.strictEqual(zvzzt.quantity, 1);
+      // Check CASH
+      const cash = portfolio.assets.find((a) => a.asset.symbol === '_CASH');
+      assert.deepEqual(cash.asset.price, $(1));
+      assert.strictEqual(cash.quantity, 50);
+    });
+    it('loads cash surplus if load amount is greater than the price of the cheapest asset but less than the price of the most underallocated asset', () => {
+      const assets = [{
+        asset: Asset('ZVZZT', $(200)),
+        quantity: 1,
+        ideal: Percent(50)
+      }, {
+        asset: Asset('ZZZZQ', $(50)),
+        quantity: 8,
+        ideal: Percent(50)
+      }];
+      let portfolio = Portfolio(assets);
+      portfolio.load($(100));
+      // Check portfolio
+      assert.deepEqual(portfolio.net, $(700));
+      assert.strictEqual(portfolio.assets.length, 3);
+      // Check SPY
+      const zvzzt = portfolio.assets.find((a) => a.asset.symbol === 'ZVZZT');
+      assert.strictEqual(zvzzt.quantity, 1);
+      // Check AGG
+      const zzzzq = portfolio.assets.find((a) => a.asset.symbol === 'ZZZZQ');
+      assert.strictEqual(zzzzq.quantity, 8);
+      // Check CASH
+      const cash = portfolio.assets.find((a) => a.asset.symbol === '_CASH');
+      assert.deepEqual(cash.asset.price, $(1));
+      assert.strictEqual(cash.quantity, 100);
+    });
+  });
+});
